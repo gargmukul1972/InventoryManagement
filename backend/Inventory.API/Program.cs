910310e907fb647611b1b26515775d1743c3f4bc
gargmukul1972 <gargmukul1972@gmail.com>
2026-06-08 20:16:00 +0530

    Apply EF Core migrations on startup

--- backend/Inventory.API/Program.cs
+++ backend/Inventory.API/Program.cs
@@ -57,4 +57,11 @@ app.MapControllers();
 
 app.MapHealthChecks("/health");
 
+using (var scope = app.Services.CreateScope())
+{
+    var db = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+
+    db.Database.Migrate();
+}
+
 app.Run();
