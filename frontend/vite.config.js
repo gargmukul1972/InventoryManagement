09d2109da9aea8c7672c9ed9bbafd500912d63ec
gargmukul1972 <gargmukul1972@gmail.com>
2026-06-09 17:46:50 +0530

    Fix microfrontend federation caching and build configuration

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -68,7 +68,9 @@ export default defineConfig({
     cssCodeSplit: false,
     rollupOptions: {
       output: {
-        assetFileNames: "assets/[name].[ext]",
+        entryFileNames: "assets/[name]-[hash].js",
+        chunkFileNames: "assets/[name]-[hash].js",
+        assetFileNames: "assets/[name]-[hash].[ext]"
       },
     },
   },
